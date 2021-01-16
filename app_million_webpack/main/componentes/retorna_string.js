8382e561cdf0d189c96daf2d052543bf19c07c95
JHugoSilva <hugosilvahs31@gmail.com>
2021-01-16 11:10:58 -0300

    Configuracao do Nodes parte 01

---
+++ app_million_webpack/main/componentes/retorna_string.js
@@ -0,0 +1,5 @@
+export const Componente = {
+    mostra: function (dado) {
+        return dado + "----"
+    }
+}
---
+++ app_million_webpack/main/index.js
@@ -0,0 +1,2 @@
+import { Componente } from './componentes/retorna_string';
+document.getElementById("content").innerHTML = `<h1>Projeto Million ${Componente.mostra("teste")}</h1>`;
